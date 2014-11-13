708927813e425e63800b9aa6f2e56700dc09cbf3
Hamid Samani <samani.hamid@gmail.com>
2014-11-14 10:40:14 +1100

    bug fix

--- app/models/teaching_strategy.rb
+++ app/models/teaching_strategy.rb
@@ -2,5 +2,5 @@ class TeachingStrategy < ActiveRecord::Base
   belongs_to :course
   attr_accessible :blended_online, :blended_online_description, :lectures, :lectures_description, :seminars, :seminars_description, :studio, :studio_description, :tutorials, :tutorials_description, :teaching_philosophy
 
-  validates_presence_of :teaching_philosophy
+  #validates_presence_of :teaching_philosophy
 end
