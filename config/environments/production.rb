596afef4eded9d73f878e0367ac204f93cc77e0c
Hamid Samani <samani.hamid@gmail.com>
2015-01-27 14:02:06 +1100

    relative_url_root added

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,9 @@ Courseoutlines::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  config.action_controller.relative_url_root  = "/course-outline"
+
   # Action mailer settings
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
