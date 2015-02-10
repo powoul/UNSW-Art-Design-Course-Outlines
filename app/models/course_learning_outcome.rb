a81c1393e19a7d9c4ae3f66797475faa3046a29f
Hamid Samani <samani.hamid@gmail.com>
2015-02-11 08:54:41 +1100

    Fixed adding empty learning outcomes

--- app/models/course_learning_outcome.rb
+++ app/models/course_learning_outcome.rb
@@ -1,6 +1,6 @@
 class CourseLearningOutcome < ActiveRecord::Base
   belongs_to :course
-  has_many :task_outcomes
+  has_many :task_outcomes, :dependent => :destroy
   has_many :assessment_tasks, :through => :task_outcomes
 
   attr_accessible :name#, :order_number
--- app/models/task_outcome.rb
+++ app/models/task_outcome.rb
@@ -1,6 +1,6 @@
 class TaskOutcome < ActiveRecord::Base
   attr_accessible :assessment_task_id, :course_learning_outcome_id#, :order_number
   belongs_to :assessment_task
-  belongs_to :course_learning_outcome
-  delegate :name, :to => :course_learning_outcome
+  belongs_to :course_learning_outcome, :dependent => :destroy
+  delegate :name, :to => :course_learning_outcome, :allow_nil => true
 end
